7ef035e0a577550ef57f89dc6cf78b0969294649
Pedro Henrique Gomes Rodrigues <pedrohrodrigues@caesb.df.gov.com>
2026-08-26 11:00:25 -0300

    projeto mini blog finalizado

--- 10_MINIBLOG/miniblogg/src/pages/CreatePost/CreatePost.js
+++ 10_MINIBLOG/miniblogg/src/pages/CreatePost/CreatePost.js
@@ -1,23 +1,65 @@
-import styles from "./CreatePost.module.css"
-import { useState } from "react"
-import {useNavigate} from "react"
-import { useAuthValue} from "react"
+import styles from "./CreatePost.module.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// context
+import { useAuthValue } from "../../context/AuthContext";
+
+// hooks
+import { useInsertDocument } from "../../hooks/useInsertDocuments";
 
 const CreatePost = () => {
   const [title, setTitle] = useState("");
   const [image, setImage] = useState("");
   const [body, setBody] = useState("");
-  const [tags, setTags] = useState([]);
+  const [tags, setTags] = useState("");
   const [formError, setFormError] = useState("");
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate();
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+
+    setFormError("");
+
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+      return;
+    }
+
+    const tagsArray = tags
+      .split(",")
+      .map((tag) => tag.trim().toLowerCase());
+
+    if (!title || !image || !body || !tags) {
+      setFormError("Por favor, preencha todos os campos!");
+      return;
+    }
+
+    setFormError("");
+
+    await insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    navigate("/");
+  };
 
   return (
     <div className={styles.create_post}>
       <h2>Criar post</h2>
       <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+
       <form onSubmit={handleSubmit}>
         <label>
           <span>Título:</span>
@@ -30,6 +72,7 @@ const CreatePost = () => {
             value={title}
           />
         </label>
+
         <label>
           <span>URL da imagem:</span>
           <input
@@ -38,9 +81,10 @@ const CreatePost = () => {
             required
             placeholder="Insira uma imagem que representa o seu post"
             onChange={(e) => setImage(e.target.value)}
-            value={title}
+            value={image}
           />
         </label>
+
         <label>
           <span>Conteúdo:</span>
           <textarea
@@ -51,6 +95,7 @@ const CreatePost = () => {
             value={body}
           ></textarea>
         </label>
+
         <label>
           <span>Tags:</span>
           <input
@@ -58,21 +103,29 @@ const CreatePost = () => {
             name="tags"
             required
             placeholder="Insira as tags separadas por vírgula"
-            onChange={(e) => setImage(e.target.value)}
+            onChange={(e) => setTags(e.target.value)}
             value={tags}
           />
         </label>
-        <button className="btn" >Cadastar</button>
-        {/*{!loading && <button className="btn">Cadastrar</button>}
-        {loading && (
-              <button className="btn" disabled>
-                Aguarde...
-                </button>
-                )}
-        {error && <p className="error">{error}</p>}} */}
+
+        {!response.loading && (
+          <button className="btn">Cadastrar</button>
+        )}
+
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+
+        {(formError || response.error) && (
+          <p className="error">
+            {formError || response.error}
+          </p>
+        )}
       </form>
     </div>
   );
-}
+};
 
-export default CreatePost
+export default CreatePost;
